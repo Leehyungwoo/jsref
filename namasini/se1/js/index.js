9b0c82d2439e769757ff8d9cbbb8211409b67fe1
dlguddn3214 <dlguddn3214@naver.com>
2019-05-05 02:04:02 +0900

    se1ep5 mystyle

--- namasini/se1/js/index.js
+++ namasini/se1/js/index.js
@@ -1,59 +1,21 @@
-//객체
-var obj = {
-    a: 1,
-    b: "abc",
-    c: function (a, b) { }
-};
-obj.a = 2;
+var elRoot = document.getElementById('elRoot');
+var result = []
 
-function myclass() {
-    this.a = 1;
-    this.b = 2;
+
+function stackSeach(searchTarget) {
+    var searchChild = searchTarget.children;
+
+    for (var i = 0, lng = searchChild.length; i < lng; i++) {
+        debugger
+        if (searchChild) {
+            result.push(searchChild[i])
+            stackSeach(searchChild[i])
+        } else {
+
+            return
+        }
+    }
+    return result
 }
 
-var arr = [];
-
-var myc = new myclass();
-console.log(obj instanceof Object)
-console.log(myc instanceof myclass)
-
-var num = 3;
-var str = "Leewoo";
-var k = str;
-k = "hi"
-console.log(str, k)
-console.log(typeof num)
-console.log(typeof str)
-
-console.log(num instanceof Object)
-console.log(str instanceof Object)
-console.log(str instanceof String)
-console.log(num instanceof Number)
-
-str.trav(function (val, index) {
-    console.log(val, index)
-})
-
-
-Number.prototype.add = function (num) {
-    return this + num
-}
-log(num.add(5))
-
-var c = [1];
-var d = c
-
-c.push(2)
-
-console.log(c)
-console.log(d)
-
-
-var obj = {
-    a: 1, b: 2, c: 3, d: 'abc'
-}
-
-var b = JSON.stringify(obj)
-var a = JSON.parse(b)
-
-console.log(obj === a)
+console.log(stackSeach(elRoot))

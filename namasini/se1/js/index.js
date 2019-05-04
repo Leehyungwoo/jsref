12189245a3f3b209d9e975af630002f59749b08d
dlguddn3214 <dlguddn3214@naver.com>
2019-05-05 02:21:49 +0900

    no message

--- namasini/se1/js/index.js
+++ namasini/se1/js/index.js
@@ -4,24 +4,17 @@ var elRoot = document.getElementById('elRoot');
 
 
 var stack = [];
-stack.push(elRoot)
 
 var res = [];
-traverse()
+traverse(document.body)
 console.log(res)
-function traverse() {
-    console.log('length', stack.length)
-    var node = stack.shift();
-    if (node) {
-        res.push(node)
-    } else {
-        return;
+
+
+function traverse(node) {
+    res.push(node)
+    var child = node.children;
+    if (child == 0) { return }
+    for (var i = 0, lng = child.length; i < lng; i++) {
+        traverse(child[i])
     }
-    var children = node.children;
-    if (children.length > 0) {
-        for (var i = 0, lng = children.length; i < lng; i++) {
-            stack.push(children[i])
-        }
-    }
-    traverse()
 }

3d9999de00ca5350d073567dc1aadf4c01eec7db
dlguddn3214 <dlguddn3214@naver.com>
2019-05-09 11:29:01 +0900

    se1ep13 nodejs-websocket

---
+++ namasini/se1/js/ws9001.js
@@ -0,0 +1,24 @@
+var ws = require("nodejs-websocket")
+var server = ws.createServer(callback).listen(9001);
+
+function callback(conn, err) {
+    if (err) {
+        //connection failted!!
+        console.dir(err)
+    } else {
+        //connection success!
+        console.log('connection success')
+    }
+
+    conn.on('text', function (str) {
+        console.log(str)
+    })
+    conn.on('error', function (err) {
+        console.dir(err)
+    })
+    conn.on('close', function (code, reason) {
+        console.dir(code)
+        console.dir(reason)
+    })
+
+}
